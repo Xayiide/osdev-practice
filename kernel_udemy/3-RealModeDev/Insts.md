15f848540e98876ec7adf919121421150c236bd4
Xayiide <wickedcheerio@protonmail.com>
2023-02-09 18:12:21 +0100

    Añadidos detalles al bootloader para manejar situaciones donde la BIOS no pone bien los registros. No podemos asumir nada. Explicado en Insts.md

---
+++ kernel_udemy/3-RealModeDev/Insts.md
@@ -0,0 +1,6 @@
+# ¿Por qué ponemos todos los registros?
+Porque puede que la BIOS los setee adecuadamente o puede que no, no lo
+sabemos. Puede que no les ponga los valores correctos (necesitamos la
+dirección de carga 0x7c00 para que todo funcione).  
+Por eso es buena práctica ponerlos nosotros y no asumir que la BIOS hace
+cualquier cosa. La BIOS puede también asumir algo que nosotros no sabemos.
--- kernel_udemy/3-RealModeDev/boot.asm
+++ kernel_udemy/3-RealModeDev/boot.asm
@@ -1,8 +1,19 @@
 ; boot.asm
-org 0x7c00
+org 0
 bits 16
 
 start:
+    cli ; desactiva las interrupciones
+    mov ax, 0x7c0 ; No podemos moverlo de una a DS ni a ES
+    mov ds, ax
+    mov es, ax
+
+    ; stack segment
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7c00
+
+    sti ; activa las interrupciones de vuelta
     mov si, msg ; la direccion de la etiqueta 'msg' a si
     call print
     jmp $ ; bucle infinito (documentacion nasm)
@@ -26,7 +37,7 @@ putchar:
     ret
     
 
-msg: db "Hola Nano, come caca", 0
+msg: db "Hola, cargabotas", 0
 
 times 510-($ - $$) db 0
 dw 0xAA55 ; little-endian
