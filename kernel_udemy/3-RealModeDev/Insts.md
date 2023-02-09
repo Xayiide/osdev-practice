a089c555d5b1c59667e4051269738719133f2779
Xayiide <wickedcheerio@protonmail.com>
2023-02-09 20:16:12 +0100

    Añadido tema de interrupciones

--- kernel_udemy/3-RealModeDev/Insts.md
+++ kernel_udemy/3-RealModeDev/Insts.md
@@ -4,3 +4,11 @@ sabemos. Puede que no les ponga los valores correctos (necesitamos la
 dirección de carga 0x7c00 para que todo funcione).  
 Por eso es buena práctica ponerlos nosotros y no asumir que la BIOS hace
 cualquier cosa. La BIOS puede también asumir algo que nosotros no sabemos.
+
+# Interrupciones
+Cuando se ponen las interrupciones, se pone el manejador *handle_int_zero*
+en los dos primeros bytes de memoria (índice 0 de la tabla de interrupts).
+Para hacer eso se pone la instrucción `mov word [ss:0x00], handle`. Si no
+se pusiera el `ss` se usaría el segmento de datos (que por el `mov` de
+más arriba contiene 0x7c0). Usamos `ss` porque arriba lo hemos puesto
+apuntando a 0x00.
--- kernel_udemy/3-RealModeDev/boot.asm
+++ kernel_udemy/3-RealModeDev/boot.asm
@@ -12,6 +12,20 @@ times 33 db 0 ; 33 octetos cero (Bios Parameter Block)
 start:
     jmp 0x7c0:step2; code segment en 0x7c0
 
+handle_int_zero:
+    mov ah, 0eh
+    mov al, 'A'  ; vamos a imprimir A
+    mov bx, 0x00
+    int 0x10
+    iret
+
+handle_int_one:
+    mov ah, 0eh
+    mov al, 'B'
+    mov bx, 0x00
+    int 0x10
+    iret
+
 step2:
     cli ; desactiva las interrupciones
     mov ax, 0x7c0 ; No podemos moverlo de una a DS ni a ES
@@ -23,6 +37,18 @@ step2:
     mov sp, 0x7c00
 
     sti ; activa las interrupciones de vuelta
+
+    ; interrupt table empieza en la dirección 0x0 de RAM
+    mov word[ss:0x00], handle_int_zero ; mueve la dir de handle_int_zero
+                                       ; a la pos. 0x00 de Stack Segment
+    mov word[ss:0x02], 0x7c0
+
+    mov word[ss:0x04], handle_int_one
+    mov word[ss:0x06], 0x7c0
+
+    int 1
+    mov ax, 0x00
+    div ax ; division por cero: int 0
 
     mov si, msg ; la direccion de la etiqueta 'msg' a si
     call print
