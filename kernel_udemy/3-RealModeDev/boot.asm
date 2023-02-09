7b4d7da56beee71684d72f5a2eb4f2c8b7ad1eb2
Xayiide <wickedcheerio@protonmail.com>
2023-02-09 18:22:32 +0100

    arreglillo

--- kernel_udemy/3-RealModeDev/boot.asm
+++ kernel_udemy/3-RealModeDev/boot.asm
@@ -2,6 +2,8 @@
 org 0
 bits 16
 
+jmp 0x7c0:start ; code segment en 0x7c0
+
 start:
     cli ; desactiva las interrupciones
     mov ax, 0x7c0 ; No podemos moverlo de una a DS ni a ES
@@ -14,6 +16,7 @@ start:
     mov sp, 0x7c00
 
     sti ; activa las interrupciones de vuelta
+
     mov si, msg ; la direccion de la etiqueta 'msg' a si
     call print
     jmp $ ; bucle infinito (documentacion nasm)
