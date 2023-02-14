4f35b2cdfa9feaadfa405dccd896c07048b67c78
Xayiide <wickedcheerio@protonmail.com>
2023-02-14 21:25:49 +0100

    Añadido código de inicialización del kernel con un driver chiquitito de disco

--- kernel_udemy/4-ProtectedModeDev/Insts.md
+++ kernel_udemy/4-ProtectedModeDev/Insts.md
@@ -7,3 +7,16 @@ layout asm
 info registers
 
 Deberíamos ver cómo bootea. En GDB, veríamos la dirección en la que estamos en 0x7c7f
+
+
+# Para ejecutar el kernel con el driver ATA:
+(desde bin/)
+gdb
+add-symbol-file ../build/kernelfull.o 0x100000
+break \_start
+target remote | qemu-system-x86\_64 -S -gdb stdio -hda ./os.bin
+c
+
+(\_start está en 0x100000)
+layout asm
+stepi (repite)
--- kernel_udemy/4-ProtectedModeDev/src/boot/boot.asm
+++ kernel_udemy/4-ProtectedModeDev/src/boot/boot.asm
@@ -31,8 +31,7 @@ step2:
     mov eax, CR0
     or eax, 0x1
     mov CR0, eax
-    ; jmp CODE_SEG:load32
-    jmp $
+    jmp CODE_SEG:load32
 
 ; Global Descriptor Table GDT
 gdt_start:
@@ -64,6 +63,66 @@ gdt_descriptor:
     dw gdt_end - gdt_start-1
     dd gdt_start
 
+[BITS 32]
+load32:
+    ; 32 bit mode
+    ; load our kernel into memory and jump to it
+    mov eax, 1   ; primer sector desde el que cargamos (0 es boot)
+    mov ecx, 100 ; cargamos 100 sectores
+    mov edi, 0x0100000 ; dirección de comienzo (donde vamos a cargarlos)
+    call ata_lba_read  ; driver de disco para cargar sectores en memoria
+    jmp CODE_SEG:0x0100000
+
+ata_lba_read:
+    mov ebx, eax ; backup LBA
+    ; enviar los 8 bits mas altos de lba al controlador de disco
+    shr eax, 24 ; despl. dcha 24 bits para quedarse con los 8 altos
+    or eax, 0xE0 ; Select the master drive
+    mov dx, 0x1F6
+    out dx, al ; Envia los 8 bits mas altos de lba
+
+    mov eax, ecx
+    mov dx, 0x1F2
+    out dx, al ; Envia el total de sectores a leer
+
+    mov eax, ebx ; restaura el LBA
+    mov dx, 0x1F3
+    out dx, al
+
+    mov dx, 0x1F4
+    mov eax, ebx ; restaura el LBA de backup
+    shr eax, 8
+    out dx, al ; Termina de enviar mas bits de LBA
+
+    ; Enviar los 16 bits mas altos de LBA
+    mov dx, 0x1F5
+    mov eax, ebx ; restaura LBA
+    shr eax, 16
+    out dx, al
+
+    mov dx, 0x1F7
+    mov al, 0x20
+    out dx, al
+
+    ; Read all sectors into memory
+.next_sector:
+    push ecx
+
+; Comprueba si necesitamos leer
+.try_again:
+    mov dx, 0x1F7
+    in al, dx ; lee del puerto 0x1F7
+    test al, 8 ; máscara
+    jz .try_again ; bucle
+
+
+    ; lee 256 palabras de una vez (1 sector)
+    mov ecx, 256
+    mov dx, 0x1F0
+    rep insw ; lee una palabra del puerto 0x1F0 y guardala en 0x010000 (ds)
+    pop ecx ; restaura ecx (del push en .next_sector)
+    loop .next_sector
+    ret
 
 
 times 510-($ - $$) db 0
