5d41bb6c0999469c855f4b15bd115732cfa0af91
Xayiide <wickedcheerio@protonmail.com>
2023-03-07 20:07:46 +0100

    Se corrige el Makefile de os para que no intente compilar código de io que ya no existe más. Además se corrige io.h para evitar el error de no inicializar las variables ret.

--- wiki_osdev/Kernel/os/io/io.h
+++ wiki_osdev/Kernel/os/io/io.h
@@ -25,21 +25,21 @@ void outl(uint16_t port, uint32_t val) {
 /* IN AL, DX : Se guarda un byte en AL del puerto en DX */
 inline __attribute__((always_inline))
 uint8_t inb(uint16_t port) {
-    uint8_t ret;
+    uint8_t ret = 0;
     __asm__ volatile("inb %1, %0" : : "a" (ret), "Nd" (port));
     return ret;
 }
 
 inline __attribute__((always_inline))
 uint16_t inw(uint16_t port) {
-    uint16_t ret;
+    uint16_t ret = 0;
     __asm__ volatile("inw %1, %0" : : "a" (ret), "Nd" (port));
     return ret;
 }
 
 inline __attribute__((always_inline))
 uint32_t inl(uint16_t port) {
-    uint32_t ret;
+    uint32_t ret = 0;
     __asm__ volatile("inl %1, %0" : : "a" (ret), "Nd" (port));
     return ret;
 }
