eee7db743751a978564f95b87014d85e4a87dcf2
Xayiide <wickedcheerio@protonmail.com>
2023-05-19 18:17:32 +0200

    Funciona. Me cago en mis muertos. :chicken:

--- JMolloy/me/src/boot/boot.asm
+++ JMolloy/me/src/boot/boot.asm
@@ -38,7 +38,7 @@ global _start
 _start:
     mov esp, stack_top
     push eax ; multiboot magic
-    ;push ebx ; multiboot heder location
+    push ebx ; multiboot heder location
     call kmain
 
     cli
--- JMolloy/me/src/kernel/kernel.c
+++ JMolloy/me/src/kernel/kernel.c
@@ -1,11 +1,26 @@
-#include "drivers/vga.h"
 #include <stdint.h>
+#include "lib/sys.h"
+#include "drivers/vga.h"
+#include "multiboot.h"
 
-void kmain(uint32_t magic)
+extern struct multiboot_header mbheader __attribute__((section(".multiboot")));
+
+void kmain(multiboot_info_t *mbd, uint32_t magic)
 {
 	/* Initialize terminal interface */
 	vga_init();
     printk("Bienvenido a ChamacOS\n");
     printk("magic: 0x%x\n", magic);
+
+    multiboot_info_t *ejemplo = mbd;
+    ejemplo++; ejemplo--;
+
+    printk("mbh magic:         0x%x\n", mbheader.magic);
+    printk("mbh header_addr:   0x%x\n", mbheader.header_addr);
+    printk("mbh load_addr:     0x%x\n", mbheader.load_addr);
+    printk("mbh load end addr: 0x%x\n", mbheader.load_end_addr);
+    printk("mbh bss_end addr:  0x%x\n", mbheader.bss_end_addr);
+
+
 }
 
